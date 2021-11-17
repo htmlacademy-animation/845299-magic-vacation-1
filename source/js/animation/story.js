61b5087b792aaa80066362b42f07ad824359672f
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-11-17 23:59:05 +0300

    fix

--- source/js/animation/story.js
+++ source/js/animation/story.js
@@ -1,5 +1,4 @@
 export const bgStoryAnimate = (linkHref) => {
-  console.log(`АниМАЦИЯ bgStoryAnimate`);
   const screenBgElement = document.getElementById(`story`).querySelector(`.screen-bg`);
   screenBgElement.classList.add(`active`);
   setTimeout(() => {
